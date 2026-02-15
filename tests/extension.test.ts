c670f97e64e561236830bc863832c94974cdda78
Sergey Kudashev <kudashevs@gmail.com>
2026-02-15 08:46:29 +0100

    feat: update basic testing

--- tests/extension.test.ts
+++ tests/extension.test.ts
@@ -1,7 +1,44 @@
+import {after, afterEach, before, beforeEach, describe, it} from 'mocha';
 import * as assert from 'assert';
+import * as vscode from 'vscode';
+import {EOL} from 'node:os';
 
-describe('VSCode compelete statement test suite', () => {
-    test('true', () => {
-      assert.equal(true, true);
+describe('VSCode complete statement extension test suite', () => {
+  let editor: vscode.TextEditor;
+
+  before(async () => {
+    const doc: vscode.TextDocument = await vscode.workspace.openTextDocument({
+      content: '',
+      language: 'plaintext'
     });
+
+    editor = await vscode.window.showTextDocument(doc);
+
+    if (!editor) {
+      assert.fail("Editor is not available");
+    }
+  });
+
+  after(async () => {
+  });
+
+  beforeEach(async () => {
+  });
+
+  afterEach(async () => {
+    await vscode.commands.executeCommand('editor.action.selectAll');
+    await vscode.commands.executeCommand('deleteLeft');
+  });
+
+  it('it can handle if', async () => {
+    await editor.edit(editBuilder => {
+      editBuilder.insert(new vscode.Position(0, 0), 'if ()');
+    });
+
+    await vscode.commands.executeCommand('extension.complete-statement');
+
+    let text = editor.document.getText();
+
+    assert.equal(text, `if () {${EOL}    ${EOL}}`, `not expected ${text}`);
+  });
 })
